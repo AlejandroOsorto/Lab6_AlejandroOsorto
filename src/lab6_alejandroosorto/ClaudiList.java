352f83d43006478577c04287359eb68384c52de6
AlejandroOsorto <73359919+AlejandroOsorto@users.noreply.github.com>
2020-11-27 14:51:45 -0600

    Añado el proyecto a github

---
+++ src/lab6_alejandroosorto/ClaudiList.java
@@ -0,0 +1,14 @@
+package lab6_alejandroosorto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ClaudiList
+{
+    private String nombre;
+    private ArrayList<Programa> lista;
+    
+    
+}
